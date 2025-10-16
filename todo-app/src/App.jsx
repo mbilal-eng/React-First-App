5b6dc00da56a6de495f50440497519869e5214ec
Muhammad Bilal <mbilal254.m@gmail.com>
2025-10-16 20:09:19 +0500

    add a todo App

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,35 +1,155 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoInput, setTodoInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  // load todos from local storage
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos) {
+      setTodoList(savedTodos);
+    }
+  }, []);
+
+  // save todos to local storage, update when todoList changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }, [todoList]);
+
+  const addTodo = () => {
+    const newTodo = {
+      id: todoList.length + 1,
+      text: todoInput,
+      completed: false,
+    };
+
+    setTodoList([...todoList, newTodo]);
+    setTodoInput("");
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todoList.filter((todo) => todo.id !== id);
+    setTodoList(updatedTodos);
+  };
+
+  const editTodo = (id) => {
+    const todoIndex = todoList.findIndex((todo) => todo.id === id);
+    const updatedText = prompt("Enter Todo", todoList[todoIndex].text);
+
+    if (!updatedText) return;
+
+    todoList[todoIndex].text = updatedText;
+    // console.log(todoList[todoIndex])
+
+    setTodoList([...todoList]);
+  };
+
+  const toggleComplete = (id) => {
+    const todoIndex = todoList.findIndex((todo) => todo.id === id);
+
+    todoList[todoIndex].completed = !todoList[todoIndex].completed;
+
+    setTodoList([...todoList]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Todo App</h1>
+      {/* Input Box */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "20px",
+          gap: "10px",
+          padding: "20px",
+          borderRadius: "10px",
+        }}
+      >
+        <input
+          style={{
+            padding: "10px",
+            fontSize: "16px",
+            width: "300px",
+            marginRight: "10px",
+            border: "1px solid #ccc",
+            borderRadius: "4px",
+          }}
+          value={todoInput}
+          onChange={(e) => setTodoInput(e.target.value)}
+          type="text"
+          placeholder="Add a new todo"
+        />
+        <button onClick={addTodo}>Add Todo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div
+        style={{
+          minHeight: "50vh",
+          background: "#0993fdff",
+          borderRadius: "10px",
+          padding: "20px",
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {todoList && <p>No Todos Yet!!!</p>}
+        {todoList.map((todo) => (
+          <div
+            key={todo.id}
+            style={{
+              width: "400px",
+              display: "flex",
+              alignItems: "center",
+              marginTop: "10px",
+            }}
+          >
+            <input
+              type="checkbox"
+              style={{
+                marginRight: "10px",
+                transform: "scale(1.5)",
+                cursor: "pointer",
+              }}
+              checked={todo.completed}
+              onChange={() => {
+                toggleComplete(todo.id);
+              }}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+                fontSize: "18px",
+              }}
+            >
+              {todo.text}
+            </span>
+            <div style={{ marginLeft: "auto", display: "flex", gap: "10px" }}>
+              <button
+                onClick={() => {
+                  editTodo(todo.id);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => {
+                  deleteTodo(todo.id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
